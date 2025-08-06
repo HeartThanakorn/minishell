fdbab38bfc67bb7c63973d43d8d1033207b99d4f
Thanakorn Thajan <tthajan@c4b1c1.42berlin.de>
2025-08-06 16:49:36 +0200

    fixed norm minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,26 +6,26 @@
 /*   By: tthajan <tthajan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 17:49:45 by tthajan           #+#    #+#             */
-/*   Updated: 2025/08/06 15:23:04 by tthajan          ###   ########.fr       */
+/*   Updated: 2025/08/06 16:48:48 by tthajan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "libft.h"
-#include "get_next_line.h"
-#include "token.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft.h"
+# include "get_next_line.h"
+# include "token.h"
 
 // Command structure for parsing
 typedef struct s_cmd
@@ -45,15 +45,15 @@ typedef struct s_cmd
 extern int	g_signal;
 
 // Built-in function declarations
-int	is_builtin(char *cmd);
-int	execute_builtin(char **args);
-int	ft_pwd(void);
-int	ft_echo(char **args);
-int	ft_exit(char **args);
-int	ft_env(char **args);
-int	ft_cd(char **args);
-int	ft_export(char **args);
-int	ft_unset(char **args);
+int		is_builtin(char *cmd);
+int		execute_builtin(char **args);
+int		ft_pwd(void);
+int		ft_echo(char **args);
+int		ft_exit(char **args);
+int		ft_env(char **args);
+int		ft_cd(char **args);
+int		ft_export(char **args);
+int		ft_unset(char **args);
 
 // Command execution
 void	exec_cmds(t_cmd *cmd_list);
@@ -69,11 +69,11 @@ void	handle_sigint(int sig);
 void	handle_sigquit(int sig);
 
 // Redirection functions
-int	redirect_input(char *filename);
-int	redirect_output(char *filename);
-int	redirect_append(char *filename);
-int	handle_heredoc(char *delimiter);
-int	create_pipe(int *pipefd);
+int		redirect_input(char *filename);
+int		redirect_output(char *filename);
+int		redirect_append(char *filename);
+int		handle_heredoc(char *delimiter);
+int		create_pipe(int *pipefd);
 
 // Pipe functions
 void	exec_pipeline(t_cmd *cmd_list);
