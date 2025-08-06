2c53f4014efb2aec56cefb081b2bff179122ab40
Thanakorn Thajan <tthajan@c4b1c1.42berlin.de>
2025-08-06 17:38:29 +0200

    fixed norm signals

--- src/signal/signals.c
+++ src/signal/signals.c
@@ -6,7 +6,7 @@
 /*   By: tthajan <tthajan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 18:02:48 by tthajan           #+#    #+#             */
-/*   Updated: 2025/08/06 15:42:11 by tthajan          ###   ########.fr       */
+/*   Updated: 2025/08/06 17:33:40 by tthajan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include <errno.h>
 #include <termios.h>
 
-// Global variable definition for signal handling
 int	g_signal = 0;
 
 void	handle_sigint(int sig)
@@ -30,28 +29,23 @@ void	handle_sigint(int sig)
 void	handle_sigquit(int sig)
 {
 	(void)sig;
-	// In interactive mode, do nothing for SIGQUIT (Ctrl-\)
 }
 
 void	setup_signals(void)
 {
-	struct	sigaction sa_int;
-	struct	sigaction sa_quit;
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
 
-	// Handle SIGINT (Ctrl-C)
 	sigemptyset(&sa_int.sa_mask);
 	sa_int.sa_handler = handle_sigint;
 	sa_int.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa_int, NULL);
-	
-	// Handle SIGQUIT (Ctrl-\) - ignore in interactive mode
 	sigemptyset(&sa_quit.sa_mask);
 	sa_quit.sa_handler = handle_sigquit;
 	sa_quit.sa_flags = SA_RESTART;
 	sigaction(SIGQUIT, &sa_quit, NULL);
 }
 
-// Set up signals for child processes
 void	setup_child_signals(void)
 {
 	signal(SIGINT, SIG_DFL);
